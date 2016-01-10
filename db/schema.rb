591e6abf498ce8b3e69fd4845c09068800a533d3
KL <git@kennethcalamay.com>
2016-01-10 18:01:56 +0800

    Creates a todolist

--- db/schema.rb
+++ db/schema.rb
@@ -14,20 +14,20 @@
 ActiveRecord::Schema.define(version: 20151122121748) do
 
   create_table "todos", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "title"
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer  "user_id",     limit: 4
+    t.string   "title",       limit: 255
+    t.string   "description", limit: 255
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "fullname"
-    t.string   "password_hash"
-    t.string   "password_salt"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.string   "username",      limit: 255
+    t.string   "fullname",      limit: 255
+    t.string   "password_hash", limit: 255
+    t.string   "password_salt", limit: 255
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
 end
