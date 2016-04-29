e2a4cc97e90bae3eb089679ca7c7c7dd64247299
Arman Jon Villalobos <me@armanjon.com>
2016-04-29 14:10:03 +0800

    modify test for creating todo item to be clearer

--- spec/concepts/todo/create_spec.rb
+++ spec/concepts/todo/create_spec.rb
@@ -1,11 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Todo::Create, type: :operation do
-  it "creates todo" do
-    todo = Todo::Create.(todo:
-      { title: "Sample Task", description: "Sample Description" }
-    ).model
+  describe "Creating todo" do
+    context "when title is empty" do
+      it "does not create the todo item" do
+        result, todo = Todo::Create.run(
+          todo: { title: nil, description: ""}
+        )
+        expect(todo.valid?).to eq false
+      end
+    end
 
-    expect(todo.title).to eq("Sample Task")
+    context "when title is present" do
+      it "creates a todo item" do
+        todo = Todo::Create.(todo:
+          { title: "Sample Task", description: "Sample Description" }
+        ).model
+
+        expect(todo.title).to eq("Sample Task")
+      end
+    end
   end
 end
