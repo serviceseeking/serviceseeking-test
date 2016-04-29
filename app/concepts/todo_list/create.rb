c8d0b7638cc09bc40dd86033f74fe79c2b48e161
Arman Jon Villalobos <me@armanjon.com>
2016-04-29 13:26:01 +0800

    add additional test for creating todo list

--- app/concepts/todo_list/create.rb
+++ app/concepts/todo_list/create.rb
@@ -9,7 +9,7 @@ class TodoList::Create < Trailblazer::Operation
   def process(params)
     validate(params[:todo_list]) do |todo_list|
       @model = TodoList.find_by_id(todo_list.id) ||
-        TodoList.find_or_create_by(name: "Default To-do List")
+        TodoList.find_or_create_by(name: todo_list.name || "Default To-do List")
       @model.user = find_or_create_user(params)
     end
   end
