bbbc486f44476e892add3469251a292605e71875
Darwin Palonpon <darwinlorenzopalonpon@gmail.com>
2016-05-17 23:04:27 +0800

    coding challenge 5/17/2016

--- spec/concepts/todo/create_spec.rb
+++ spec/concepts/todo/create_spec.rb
@@ -1,7 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Todo::Create do
-  it "can be spec'd" do
-    raise "Implement me"
+
+class Todo::Create
+  public :require
+end
+
+  context "when given a todo with no title" do
+    todo_params = ActionController::Parameters.new(todo: {title: nil})
+    op = Todo::Create.(todo_params)
+    it "should not create a todo item" do
+      expect(op.model.id).to be_nil
+    end
   end
+
+  context "when given a todo with title" do
+    todo_params = ActionController::Parameters.new(todo: {title: "something"})
+    op = Todo::Create.(todo_params)
+
+    it "should create a todo item" do
+      expect(op.model.id).not_to be_nil
+    end
+
+    it "should belong to a todo list" do
+      expect(op.model.todo_list_id).not_to be_nil
+    end
+
+  end
+
 end
