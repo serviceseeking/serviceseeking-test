47a2f75fda319aa6e93273fdf49b784b8ab074a4
Carlos Molo <cvmolo03@gmail.com>
2016-08-15 02:51:50 +0800

    Update rspec tests for Todo::Create

--- spec/concepts/todo/create_spec.rb
+++ spec/concepts/todo/create_spec.rb
@@ -9,7 +9,21 @@ RSpec.describe Todo::Create do
     expect(todo.model.title).to eq("Title 1")
     expect(todo.model.description).to eq("Desc 1")
     expect(todo.model.list.name).to eq("Default To-do List")
+    expect(todo.model.list.user.fullname).to eq("Guest")
   end
+
+	it "creates a valid Todo with a user and todo list" do
+		user = User.create!(fullname: "User 2")
+		todo_list = TodoList.create!(name: "List 2", user_id: user.id)
+    res, todo = Todo::Create.run(todo: {title: "Title 2", description: "Desc 2"}, todo_list_id: todo_list.id)
+
+    expect(res).to be true
+    expect(todo.model.persisted?).to be true
+    expect(todo.model.title).to eq("Title 2")
+    expect(todo.model.description).to eq("Desc 2")
+    expect(todo.model.list.name).to eq("List 2")
+    expect(todo.model.list.user.fullname).to eq("User 2")
+  end  
 
   it "does not create an invalid Todo" do
 	res, todo = Todo::Create.run(todo: {title: ""})
